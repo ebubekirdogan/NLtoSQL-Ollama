c007f67c62ceb3258af0abfaae59ca686b2e2707
Ebubekir Dogan <ebubekir.dogan@ogr.sakarya.edu.tr>
2026-09-11 10:48:27 +0300

    Initial commit

---
+++ test_ollama.py
@@ -0,0 +1,13 @@
+import ollama
+
+response = ollama.chat(      # terminalden yaptigimiz konusma islemini  Python'dan otomatik olarak yapiyor. Ollama'ya arka plandan bir istek atar.
+    model="qwen2.5-coder:7b",
+    # bir liste ile modele gonderilecek mesajlar belirlenir.
+    # role: user -> kullanicidan gelen mesaj oldugunu modele bildirir.
+    # content: user -> kullanicidan gelen mesajin icerigini belirler.
+    messages=[
+        {"role": "user", "content": "Write a SQL query to select all rows from a table called machines."}
+    ]
+)
+# response message ile modelin mesaj kismina gidilir ve de content ile modelin urettigi "ham cikti" alinir.
+print(response["message"]["content"]) 
